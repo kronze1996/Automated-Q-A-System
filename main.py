9621a5609f558805928b58e8cd56b8fd464cb910
Kartikey Sharma <kartikkey13@gmail.com>
2021-05-07 14:45:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,53 @@
+import pickle
+import ir
 # import textCleaning
-# import ir
-# import datasetMaker
-# import textCleaning
-# import tf_IDF
+from gensim import models
+from gensim import similarities
 import streamlit as st
+import tf_IDF
+from gensim import corpora
+from gensim.parsing import strip_tags, strip_numeric, \
+    strip_multiple_whitespaces, stem_text, strip_punctuation, \
+    remove_stopwords, preprocess_string
+import QuesAns
 
 # Project Title
-header = st.beta_container()
+header= st.beta_container()
 with header:
     st.title("Welcome to Automated Q&A bot")
 
-# To fetch the query that is the question
-question = st.text_input('Ask a question here:')
-# if question:
-#     st.write(my_model.predict(sentence))
+# To fetch the query that is the question  
+question=st.text_input('Ask a question here:') 
+if question:
+  with open('C:/Users/karti/OneDrive/Documents/Automated_Q_A/clean_text.pkl', 'rb') as p:
+    new_docs = pickle.load(p)
+  text_docs=[]    
+  for ele in new_docs:
+    if len(ele) > 50:
+      text_docs.append(ele)
+    else:
+      pass
+  # Loading all pickle files
+  #dictionary, pdoc = ir.create_dictionary(raw_corpus)
+  with open('C:/Users/karti/OneDrive/Documents/Automated_Q_A/dictionary.pkl', 'rb') as p:
+    dictionary = pickle.load(p)
+  with open('C:/Users/karti/OneDrive/Documents/Automated_Q_A/index.pkl', 'rb') as p:
+    index = pickle.load(p)
+  with open('C:/Users/karti/OneDrive/Documents/Automated_Q_A/tfidf.pkl', 'rb') as p:
+    tfidf = pickle.load(p)
+  
+  answer=[]
+  for d in tf_IDF.get_closest_n(question,5, dictionary, index, tfidf, text_docs):
+      answer.append(d)
+  Answer=st.beta_container()
+  with Answer:
+      st.title("The answer")
+      st.write(answer)
+      
+  ans=QuesAns.QuesAnswer(question,answer)
 
-# # Getting the raw corpus
-# pdfList = ["Copy of Applied Data Science.pdf"]
-# raw_corpus = datasetMaker.DM(pdfList)
-# corpus = textCleaning.cleaning_pipe(raw_corpus)
-# dictionary, pdoc = ir.create_dictionary(corpus)
-# bow_corpus = ir.transform_corpus(pdoc, dictionary)
-# index, tfidf = tf_IDF.tf_idf(bow_corpus, dictionary)
-# answer = ''
-# for d in tf_IDF.get_closest_n("What is Data Science", 2, dictionary, index, tfidf, raw_corpus):
-#     answer += d
-
-
-# To fetch the predicted result of the query
-Answer = st.beta_container()
-with Answer:
-    st.title("The answer")
-    # st.write(answer)
+  # To fetch the predicted result of the query
+  Answer=st.beta_container()
+  with Answer:
+      st.title("The answer")
+      st.write(ans)
